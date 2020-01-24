e596855d332a3724dcf79bdea10684fb3639dd5b
Berta Cume <bertacume@gmail.com>
2020-01-24 12:25:24 +0100

    copy real tp

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// Clonator (30 mins)
+
+// Write a function that can create a deep copy of anything
+// it gets passed. The generated copy should have the same own
+// enumerable properties as the source, but modifying
+// the copy should not affect the source in any way.
+
+function clonator (source) {
+
+}
+
+module.exports = clonator;
